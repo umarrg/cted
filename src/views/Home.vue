840dcc2707a120065abcb0e9e7d2aadf12d31f7b
Mubarak Ibrahim <elmubarak333@gmail.com>
2021-02-18 06:38:27 +0100

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <v-app>
-    <v-carousel cycle color="" height="100%" hide-delimiter-background>
-      <v-carousel-item>
         <v-sheet color="#f2f5f8" height="100%">
-          <v-container>
+          <v-container height="100%">
             <v-row>
               <v-col cols="12" md="6" class="mt-12">
                 <div class="mt-8">
@@ -22,94 +20,23 @@
                   </p>
                 </div>
 
-                <!-- <div class="mt-8">
-                  <v-btn color="#59BBBB" large class="white--text" href="#hero">
-                    Get Started
-                  </v-btn>
-                </div> -->
+                
               </v-col>
-              <v-col cols="12" md="6" lg="6" class="mt-8 mb-6">
+              <v-col cols="12" md="6" lg="6" class="mt-5 mb-5">
                 <div>
                   <v-img src="../assets/ca1.svg"></v-img>
                 </div>
               </v-col>
             </v-row>
-          </v-container>
-        </v-sheet>
-      </v-carousel-item>
-      <v-carousel-item>
-        <v-sheet color="#f2f5f8" height="100%">
-          <v-container>
-            <v-row>
-              <v-col cols="12" md="6" lg="6" class="mt-12">
-                <div class="mt-8">
-                  <h1 class="display-2 text__one">
-                    CENTRE FOR TRADE AND ENTERPRISE DEVELOPMENT
-                    <span class="custom__color">(CTED)</span>
-                  </h1>
-                </div>
 
-                <div class="mt-12">
-                  <p class="custom__p mt-8">
-                    TECHNICAL SUPPORT TO CAPACITY BUILDING FOR EXPORT PROMOTION
-                    FOR MSMEs AND YOUTH OWNED BUSINESSES IN NIGERIA TOWARDS
-                    INCREASING THE COUNTRY’S BENEFIT FROM AFCFTA & OTHER ONGOING
-                    BILATERAL AGREEMENTS.
-                  </p>
-                </div>
-
-                <!-- <div class="mt-8">
-                  <v-btn color="#59BBBB" large class="white--text" href="#hero">
-                    Get Started
+            <div class="mt-0 d-flex justify-center align-center" >
+                  <v-btn color="#59BBBB" fab elevation="1" class="white--text" href="#hero">
+                  <v-icon>mdi-chevron-double-down</v-icon>
                   </v-btn>
-                </div> -->
-              </v-col>
-              <v-col cols="12" md="6" lg="6" class="mt-8 mb-6">
-                <div>
-                  <v-img src="../assets/ca3.svg"></v-img>
                 </div>
-              </v-col>
-            </v-row>
           </v-container>
         </v-sheet>
-      </v-carousel-item>
-      <v-carousel-item>
-        <v-sheet color="#f2f5f8" height="100%">
-          <v-container>
-            <v-row>
-              <v-col cols="12" md="6" lg="6" class="mt-12">
-                <div class="mt-8">
-                  <h1 class="text__one display-2">
-                    CENTRE FOR TRADE AND ENTERPRISE DEVELOPMENT
-                    <span class="custom__color">(CTED)</span>
-                  </h1>
-                </div>
-
-                <div class="mt-12">
-                  <p class="custom__p mt-8">
-                    TECHNICAL SUPPORT TO CAPACITY BUILDING FOR EXPORT PROMOTION
-                    FOR MSMEs AND YOUTH OWNED BUSINESSES IN NIGERIA TOWARDS
-                    INCREASING THE COUNTRY’S BENEFIT FROM AFCFTA & OTHER ONGOING
-                    BILATERAL AGREEMENTS.
-                  </p>
-                </div>
-                <!-- 
-                <div class="mt-8">
-                  <v-btn color="#59BBBB" large class="white--text" href="#hero">
-                    Get Started
-                  </v-btn>
-                </div> -->
-              </v-col>
-              <v-col cols="12" md="6" lg="6" class="mt-8 mb-6">
-                <div>
-                  <v-img src="../assets/ca4.svg"></v-img>
-                </div>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel>
+      
 
     <Hero id="hero" />
     <Section id="section" />
